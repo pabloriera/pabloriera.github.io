7184b05078fefa446fd0b21724e5834693cdb156
pabloriera <pablo.riera@gmail.com>
2017-04-14 03:44:48 -0300

    minor

--- export/html_nocode.py
+++
@@ -1,8 +0,0 @@
-c = get_config()
-
-#Export all the notebooks in the current directory to the sphinx_howto format.
-c.NbConvertApp.notebooks = ['*.ipynb']
-c.NbConvertApp.export_format = 'html'
-c.TemplateExporter.template_path = ['.']
-# c.exclude_output_prompt = True
-c.Exporter.template_file = 'custom.tpl'
